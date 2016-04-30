9ee6898745ad4774917354c56430fcaedbd32e63
Daniel Jones <xXDJONESXx@hotmail.com>
2016-04-30 13:09:49 +1000

    Fixed up and down arrow keys with scroll axis

--- bolt/Editor.cs
+++ bolt/Editor.cs
@@ -5,7 +5,8 @@ namespace bolt
 {
 	public class Editor : GraphicalInterface, InputListener
 	{
-		public static int TAB_SPACES => ((bool)Settings.settings["SHOW_LINE_NUMBERS"]) ? 8 : 0;
+		//public static int TAB_SPACES => ((bool)Settings.settings["SHOW_LINE_NUMBERS"]) ? 8 : 0;
+		public static int TAB_SPACES = 8;
 		public int Scroll = 0;
 		public Location cursor = new Location(0, 0);
 		public List<string> lines;
@@ -22,17 +23,16 @@ namespace bolt
 
 		public override void Update ()
 		{
-			int i = Scroll;
+			int i = 0;
 			for (; i < lines.Count && i < size.Height; i++)
 			{
 				//TODO Syntax Highlighting
 				GUI.ClearLine(i);
 				bool showLineNumbers = (bool)Settings.settings ["SHOW_LINE_NUMBERS"];
-				GUI.DrawString((showLineNumbers ? (i + ":\t") : "") + lines[i], new Location(0, i));
+				GUI.DrawString((showLineNumbers ? ((i + Scroll) + ":\t") : "") + lines[i + Scroll], new Location(0, i));
 			}
 			for (; i < size.Height; i++)
 				GUI.ClearLine(i);
-			//ASSUMING TABS ARE 4 SPACES
 			cursor.X += TAB_SPACES;
 			GUI.SetCursorPos (cursor);
 			cursor.X -= TAB_SPACES;
@@ -45,22 +45,37 @@ namespace bolt
 				return;
 			}
 			if (keyInfo.Key == ConsoleKey.UpArrow) {
-				if (cursor.Y > 0) {
-					cursor.Y--;
-					if (cursor.X > lines [cursor.Y].Length) {
-						cursor.X = lines [cursor.Y].Length;
-					} else if (XWant > lines [cursor.Y].Length)
-						cursor.X = lines [cursor.Y].Length;
+				cursor.Y--;
+				if (cursor.Y + Scroll >= 0) {
+					if (cursor.X > lines [cursor.Y + Scroll].Length) {
+						cursor.X = lines [cursor.Y + Scroll].Length;
+						if (cursor.Y == -1) {
+							cursor.Y++;
+							Scroll--;
+						}
+					} else if (cursor.Y == -1 && Scroll > 0) {
+						Scroll--;
+						cursor.Y++;
+						SelfUpdate ();
+					}
+					if (XWant > lines [cursor.Y + Scroll].Length)
+						cursor.X = lines [cursor.Y + Scroll].Length;
 					else
 						cursor.X = XWant;
-				}
+				} else
+					cursor.Y = 0;
 			} else if (keyInfo.Key == ConsoleKey.DownArrow) {
-				if (cursor.Y < lines.Count - 1) {
+				if (cursor.Y + Scroll < lines.Count - 1) {
 					cursor.Y++;
-					if (cursor.X > lines [cursor.Y].Length) {
-						cursor.X = lines [cursor.Y].Length;
-					} else if (XWant > lines [cursor.Y].Length)
-						cursor.X = lines [cursor.Y].Length;
+					if (cursor.X > lines [cursor.Y + Scroll].Length) {
+						cursor.X = lines [cursor.Y + Scroll].Length;
+					} else if (cursor.Y > size.Height - 1) {
+						Scroll++;
+						cursor.Y--;
+						SelfUpdate ();
+					}
+					if (XWant > lines [cursor.Y + Scroll].Length)
+						cursor.X = lines [cursor.Y + Scroll].Length;
 					else
 						cursor.X = XWant;
 				}
@@ -70,20 +85,21 @@ namespace bolt
 					XWant = cursor.X;
 				} else if (cursor.Y > 0) {
 					cursor.Y--;
-					cursor.X = lines [cursor.Y].Length;
+					cursor.X = lines [cursor.Y + Scroll].Length;
 				}
 			} else if (keyInfo.Key == ConsoleKey.RightArrow) {
-				if (cursor.X < lines [cursor.Y].Length) {
+				if (cursor.X < lines [cursor.Y + Scroll].Length) {
 					cursor.X++;
 					XWant = cursor.X;
 				}
-				else if (cursor.Y < lines.Count-1) {
+				else if (cursor.Y + Scroll < lines.Count-1) {
 					cursor.Y++;
+					Scroll++;
 					cursor.X = 0;
 				}
 			} else if (keyInfo.Key == ConsoleKey.Backspace) {
 				if (cursor.X > 0 && cursor.X < lines [cursor.Y].Length + 1) {
-					lines [cursor.Y] = lines [cursor.Y].Remove (cursor.X - 1, 1);
+					lines [cursor.Y + Scroll] = lines [cursor.Y + Scroll].Remove (cursor.X - 1, 1);
 					cursor.X--;
 					XWant = cursor.X;
 					codeFile.Changed = true;
@@ -92,7 +108,7 @@ namespace bolt
 				} else if (cursor.X == 0 && cursor.Y != 0)
 				{
 					int width = lines [cursor.Y - 1].Length;
-					lines [cursor.Y - 1] += lines [cursor.Y];
+					lines [(cursor.Y + Scroll) - 1] += lines [cursor.Y + Scroll];
 					lines.RemoveAt (cursor.Y);
 					cursor.Y--;
 					cursor.X = width;
@@ -100,25 +116,24 @@ namespace bolt
 				}
 			} else if (keyInfo.Key == ConsoleKey.Enter) {
 				string[] split = new string[2];
-				split [0] = lines [cursor.Y].Substring (0, cursor.X);
-				split [1] = lines [cursor.Y].Substring (cursor.X);
-				lines.Insert (cursor.Y, split[0]);
+				split [0] = lines [cursor.Y + Scroll].Substring (0, cursor.X);
+				split [1] = lines [cursor.Y + Scroll].Substring (cursor.X);
+				lines.Insert (cursor.Y + Scroll, split[0]);
 				cursor.Y++;
-				lines [cursor.Y] = split [1];
+				lines [cursor.Y + Scroll] = split [1];
 				cursor.X = 0;
 				codeFile.Changed = true;
 				SelfUpdate ();
 				bolt.statusBar.SelfUpdate ();
 			}
 			else {
-				lines [cursor.Y] = lines [cursor.Y].Insert (cursor.X, "" + keyInfo.KeyChar);
+				lines [cursor.Y + Scroll] = lines [cursor.Y + Scroll].Insert (cursor.X, "" + keyInfo.KeyChar);
 				cursor.X++;
 				codeFile.Changed = true;
 				SelfUpdate ();
 				bolt.statusBar.SelfUpdate ();
 			}
 			bolt.CurrentlyUpdating = this;
-			//ASSUMING TABS ARE 4 SPACES
 			cursor.X += TAB_SPACES;
 			GUI.SetCursorPos (cursor);
 			cursor.X -= TAB_SPACES;
