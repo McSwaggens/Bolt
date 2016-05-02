302910e8c8fb48e21b582cf75c646c74df56c420
Daniel Jones <xXDJONESXx@hotmail.com>
2016-05-02 13:36:17 +1000

    Fixed cursor movement, scrolling and editing

--- bolt/Editor.cs
+++ bolt/Editor.cs
@@ -8,7 +8,7 @@ namespace bolt
 		//public static int TAB_SPACES => ((bool)Settings.settings["SHOW_LINE_NUMBERS"]) ? 8 : 0;
 		public static int TAB_SPACES = 8;
 		public int Scroll = 0;
-		public Location cursor = new Location(0, 0);
+		public Location cursor = new Location(0, 20);
 		public List<string> lines;
 		public CodeFile codeFile;
 		public int XWant = 0;
@@ -24,7 +24,7 @@ namespace bolt
 		public override void Update ()
 		{
 			int i = 0;
-			for (; i < lines.Count && i < size.Height; i++)
+			for (; i + Scroll < lines.Count && i < size.Height; i++)
 			{
 				//TODO Syntax Highlighting
 				GUI.ClearLine(i);
@@ -45,60 +45,69 @@ namespace bolt
 				return;
 			}
 			if (keyInfo.Key == ConsoleKey.UpArrow) {
-				cursor.Y--;
-				if (cursor.Y + Scroll >= 0) {
-					if (cursor.X > lines [cursor.Y + Scroll].Length) {
-						cursor.X = lines [cursor.Y + Scroll].Length;
-						if (cursor.Y == -1) {
-							cursor.Y++;
-							Scroll--;
-						}
-					} else if (cursor.Y == -1 && Scroll > 0) {
+				if (cursor.Y == 0) {
+					if (Scroll != 0) {
 						Scroll--;
-						cursor.Y++;
+						if (XWant > lines [cursor.Y + Scroll].Length)
+							cursor.X = lines [cursor.Y + Scroll].Length;
+						else
+							cursor.X = XWant;
 						SelfUpdate ();
 					}
+				} else {
+					cursor.Y--;
 					if (XWant > lines [cursor.Y + Scroll].Length)
 						cursor.X = lines [cursor.Y + Scroll].Length;
 					else
 						cursor.X = XWant;
-				} else
-					cursor.Y = 0;
+				}
 			} else if (keyInfo.Key == ConsoleKey.DownArrow) {
-				if (cursor.Y + Scroll < lines.Count - 1) {
-					cursor.Y++;
-					if (cursor.X > lines [cursor.Y + Scroll].Length) {
-						cursor.X = lines [cursor.Y + Scroll].Length;
-					} else if (cursor.Y > size.Height - 1) {
+				if (cursor.Y == size.Height-1) {
+					if (cursor.Y + Scroll != lines.Count-1) {
 						Scroll++;
-						cursor.Y--;
+						if (XWant > lines [cursor.Y + Scroll].Length)
+							cursor.X = lines [cursor.Y + Scroll].Length;
+						else
+							cursor.X = XWant;
 						SelfUpdate ();
 					}
+				} else {
+					cursor.Y++;
 					if (XWant > lines [cursor.Y + Scroll].Length)
 						cursor.X = lines [cursor.Y + Scroll].Length;
 					else
 						cursor.X = XWant;
 				}
 			} else if (keyInfo.Key == ConsoleKey.LeftArrow) {
-				if (cursor.X != 0) {
+				if (cursor.X == 0) {// Check if the cursor is at the bottom of the line
+					if (Scroll != 0 && cursor.Y == 0) {
+						Scroll--;
+						cursor.Y--;
+						cursor.X = lines [cursor.Y + Scroll].Length;
+					} else if (cursor.Y != 0) {
+						cursor.Y--;
+						cursor.X = lines [cursor.Y + Scroll].Length;
+					}
+				} else { // Cursor is not at the bottom of the line
 					cursor.X--;
-					XWant = cursor.X;
-				} else if (cursor.Y > 0) {
-					cursor.Y--;
-					cursor.X = lines [cursor.Y + Scroll].Length;
+					XWant = cursor.X; // Set the XWant
 				}
 			} else if (keyInfo.Key == ConsoleKey.RightArrow) {
-				if (cursor.X < lines [cursor.Y + Scroll].Length) {
+				if (cursor.X == lines [cursor.Y + Scroll].Length) {
+					if (Scroll + cursor.Y < lines.Count-1 && cursor.Y == size.Height) {
+						Scroll++;
+						cursor.Y++;
+						cursor.X = 0;
+					} else if (cursor.Y + Scroll < lines.Count-1) {
+						cursor.Y++;
+						cursor.X = 0;
+					}
+				} else {
 					cursor.X++;
 					XWant = cursor.X;
 				}
-				else if (cursor.Y + Scroll < lines.Count-1) {
-					cursor.Y++;
-					Scroll++;
-					cursor.X = 0;
-				}
 			} else if (keyInfo.Key == ConsoleKey.Backspace) {
-				if (cursor.X > 0 && cursor.X < lines [cursor.Y].Length + 1) {
+				if (cursor.X > 0 && cursor.X < lines [cursor.Y + Scroll].Length + 1) {
 					lines [cursor.Y + Scroll] = lines [cursor.Y + Scroll].Remove (cursor.X - 1, 1);
 					cursor.X--;
 					XWant = cursor.X;
@@ -107,9 +116,9 @@ namespace bolt
 					bolt.statusBar.SelfUpdate ();
 				} else if (cursor.X == 0 && cursor.Y != 0)
 				{
-					int width = lines [cursor.Y - 1].Length;
+					int width = lines [(cursor.Y + Scroll)- 1].Length;
 					lines [(cursor.Y + Scroll) - 1] += lines [cursor.Y + Scroll];
-					lines.RemoveAt (cursor.Y);
+					lines.RemoveAt (cursor.Y + Scroll);
 					cursor.Y--;
 					cursor.X = width;
 					SelfUpdate ();
