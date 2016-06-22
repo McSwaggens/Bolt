030daff63fae9d61529777551958e04c0eb9df48
Daniel Jones <xXDJONESXx@hotmail.com>
2016-06-22 13:28:32 +1000

    Added notification API

---
+++ bolt/NotificationHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace bolt
+{
+	public class NotificationHandler
+	{
+		private static ConsoleColor[] notificationColors =
+		{
+			ConsoleColor.Gray,
+			ConsoleColor.Yellow,
+			ConsoleColor.Red
+		};
+		
+		private static char[] notificationChars =
+		{
+			'#',
+			'?',
+			'!'
+		};
+		
+		public static void Push (string message)
+		{
+			if (Bolt.instance == null)
+				return;
+			
+			Bolt.instance.commandPanel.PushNotification( '~' + message, ConsoleColor.Gray);
+		}
+		
+		public static void Push (string message, ConsoleColor color)
+		{
+			if (Bolt.instance == null)
+				return;
+			
+			Bolt.instance.commandPanel.PushNotification( '~' + message, color);
+		}
+		
+		public static void Push (string message, NotificationType type)
+		{
+			if (Bolt.instance == null)
+				return;
+			
+			Bolt.instance.commandPanel.PushNotification( '~' + notificationChars[(int)type] + message, ConsoleColor.Gray);
+		}
+	}
+}
+
---
+++ bolt/NotificationType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace bolt
+{
+	public enum NotificationType
+	{
+		NORMAL = 0x0,
+		
+		WARNING = 0x1,
+		
+		ERROR = 0x2,
+		
+		
+	}
+	
+}
+
